ef068bd98b0fd5903333ce42cd923f3fff1f2d9f
Isa Goksu <isa.goksu@gmail.com>
2013-11-19 23:33:25 +0200

    A sample application, classical hello world

---
+++ src/main/java/bist/chapter01/Hello.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * This code is developed and owned by Borsa Istanbul A.S.
+ * The distribution of this code by any means is prohibited.
+ * You cannot copy/share/distribute/deliver/use/modify/move without having
+ * a permission to do so. Use it with your own risk.
+ *
+ * All rights reserved. Copyright (c) 2013.
+ ******************************************************************************/
+
+package bist.chapter01;
+
+import com.sun.istack.internal.Nullable;
+
+/**
+ * @version 1.0
+ */
+public class Hello {
+
+   public static void main(@Nullable String[] args) {
+
+      System.out.println("Selam");
+
+   }
+
+}
