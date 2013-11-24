319038d236705708e28e573978105cceee8e08ae
Isa Goksu <isa.goksu@gmail.com>
2013-11-24 03:17:42 +0200

    Basics of strings

---
+++ src/main/java/bist/chapter02/Strings.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * This code is developed and owned by Borsa Istanbul A.S.
+ * The distribution of this code by any means is prohibited.
+ * You cannot copy/share/distribute/deliver/use/modify/move without having
+ * a permission to do so. Use it with your own risk.
+ *
+ * All rights reserved. Copyright (c) 2013.
+ ******************************************************************************/
+
+package bist.chapter02;
+
+/**
+ * @version 1.0
+ */
+public class Strings {
+
+   public static void main(String[] args) {
+      String name = "John";
+      String escapes = "John's quote: \"Something\"";
+      String newLine = "\nA paragraph\nAnother paragraph";
+
+      System.out.println("name = " + name);
+      System.out.println("escapes = " + escapes);
+      System.out.println("newLine = " + newLine);
+   }
+
+}
