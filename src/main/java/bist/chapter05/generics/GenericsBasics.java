2a4d98cf23b7eb48b5eba083fc0085cef8354a9e
Isa Goksu <isa.goksu@gmail.com>
2013-12-02 10:00:25 +0200

    Added copyrights and homework templates

--- src/main/java/bist/chapter05/generics/GenericsBasics.java
+++ src/main/java/bist/chapter05/generics/GenericsBasics.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * This code is developed and owned by Borsa Istanbul A.S.
+ * The distribution of this code by any means is prohibited.
+ * You cannot copy/share/distribute/deliver/use/modify/move without having
+ * a permission to do so. Use it with your own risk.
+ *
+ * All rights reserved. Copyright (c) 2013.
+ ******************************************************************************/
+
 package bist.chapter05.generics;
 
 
