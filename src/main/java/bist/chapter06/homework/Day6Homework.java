7614b3dacf52d218e0af3e46743abe6f438174a6
Isa Goksu <isa.goksu@gmail.com>
2013-12-03 17:11:11 +0200

    Added homework

---
+++ src/main/java/bist/chapter06/homework/Day6Homework.java
@@ -0,0 +1,30 @@
+package bist.chapter06.homework;
+
+public class Day6Homework {
+	/**
+	 * OBJECTIVE:
+	 * 
+	 * A simple DB table (called Employee) and a DB Shell will be simulated in the homework.
+	 * 
+	 * DB table should contain the below fields:
+	 * - ID (long): ID fields must be unique.
+	 * - FirstName (String)
+	 * - LastName (String)
+	 * - Age (int)
+	 * - BirthDate (Date): Dates should be stored in YYYYMMDD format. Example date: 19800504
+	 * - Salary (BigDecimal)
+	 * 
+	 * User should interact with the DB using a CLI-based shell. Shell should enlist the available operations
+	 * and should guide the user during the process.
+	 * - findAllEmployees();
+	 * - findEmployeeById(long employeeId);
+	 * - createEmployee(Employee employee);
+	 * - deleteEmployeeById(long employeeId);
+	 * - updateSalary(long employeeId, BigDecimal newSalary); 
+	 * 
+	 * Result of the operations should be printed to the screen. Employee data should be preserved in a CSV file,
+	 * such as employee.csv.
+	 * 
+	 * Good luck!
+	 */
+}
