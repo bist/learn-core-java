8d1add0388fe61ad51d51d0705ca2af5348e3c9c
Isa Goksu <isa.goksu@gmail.com>
2013-12-09 08:35:55 +0200

    Added homework

---
+++ src/main/java/bist/chapter08/homework/Day8Homework.java
@@ -0,0 +1,22 @@
+package bist.chapter08.homework;
+
+public class Day8Homework {
+	/**
+	 * OBJECTIVE:
+	 * 
+	 * A simple DB table (called Employee) and a DB Shell will be implemented in the homework, as in Homework 6. 
+	 * But, this time DB table, Employee, will be created in HSQLDB and the DB shell will be able to connect  
+	 * this DB remotely. 
+	 * 
+	 * Multiple shells can be opened to the database.
+	 * 
+	 * DB table should contain the exact same fields and the DB shell should support the exact same operations, 
+	 * mentioned in Homework 6.
+	 * 
+	 * To support remote communication between DB shells and the database, you might the need to implement a
+	 * custom protocol to differentiate different operations and to demarcate the beginning and the end of 
+	 * the exchanged messages.
+	 *  
+	 * Good luck!
+	 */
+}
